922b2dcb4dee05f922091818b382d24016d8b058
Giacomo Lancerin <lanceringiacomo@gmail.com>
2026-04-30 18:14:36 +0200

    chore: switch font to Bricolage Grotesque, refine UI aesthetics
    Replace DM Sans with Bricolage Grotesque variable font (optical sizing
    auto). Improve card contrast (white bg, subtle border), editorial
    eyebrow with red accent line, larger hero title, warmer tag palette.

--- website/pages/_document.js
+++ website/pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;0,9..40,800&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,200..800&display=swap"
           rel="stylesheet"
         />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
